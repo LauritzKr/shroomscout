1fead76994fdb6bf99597f6a79c19ba8a84e7527
Sant1ag0x00 <tim@weltraumkatzen.space>
2024-02-29 16:59:19 +0100

    add code comments

--- backend/get_finds.py
+++ backend/get_finds.py
@@ -2,15 +2,22 @@ import sqlite3
 
 def get_finds():
     findlist = []
+
+    #establish database connection
     con = sqlite3.connect("../database/shroomscout")
     cur = con.cursor()
     cur.execute("SELECT * FROM findLog")
     rows = cur.fetchall()
     columns = [column[0] for column in cur.description]
+    
+    #append each row to findList as a dictionary
     for row in rows:
         row_dict = dict(zip(columns, row))
         findlist.append(row_dict)
     con.close()
+    
+    #enclose array of objects in another object in order to meet json standards 
+    #and enable sending metadata in the future
     return {"data": findlist}
 
 if __name__ == "__main__":
--- backend/get_shrooms.py
+++ backend/get_shrooms.py
@@ -2,15 +2,22 @@ import sqlite3
 
 def get_shrooms():
     shroomlist = []
+
+    #establish database connection
     con = sqlite3.connect("../database/shroomscout")
     cur = con.cursor()
     cur.execute("SELECT * FROM mushrooms")
     rows = cur.fetchall()
     columns = [column[0] for column in cur.description]
+
+    #append each row to findList as a dictionary
     for row in rows:
         row_dict = dict(zip(columns, row))
         shroomlist.append(row_dict)
     con.close()
+
+    #enclose array of objects in another object in order to meet json standards 
+    #and enable sending metadata in the future
     return {"data": shroomlist}
 
 
